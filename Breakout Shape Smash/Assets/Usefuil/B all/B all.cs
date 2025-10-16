95531deb605952b87ea7bd2df8427d6281cc6992
jlay-tp <jlaytin@gmail.com>
2025-10-16 19:42:29 -0400

    Colliders

--- Breakout Shape Smash/Assets/Usefuil/B all/B all.cs
+++ Breakout Shape Smash/Assets/Usefuil/B all/B all.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float xspeed = 5.0f;
+    [SerializeField] float yspeed = 5.0f;
+
     void Start()
     {
         
@@ -11,6 +14,20 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += new Vector3(xspeed, yspeed, 0) * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.ToString());
+        if (other.gameObject.tag == "side")
+        {
+            xspeed *= -1;
+
+        }
+        else if (other.gameObject.tag == "top")
+        {
+            yspeed = yspeed * -1;
+        }
     }
 }
